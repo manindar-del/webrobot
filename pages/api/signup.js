742a200606c1e1a7aa602df85d1f622ecae34b34
unknown <kaushik2017.mukherjee@gmail.com>
2024-05-12 01:19:51 +0530

    design

---
+++ pages/api/signup.js
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+// import { corsMiddleware, jsonParser } from "../../middleware"; // If you have custom middleware
+import corsMiddleware from "../../cors";
+
+let prisma = new PrismaClient();
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient({
+      log: ["query", "info", "warn", "error"],
+    });
+  }
+  prisma = global.prisma;
+}
+
+prisma.$on("query", (e) => {
+  console.log(`${e.timestamp} Query: ${e.query}`);
+  console.log(`${e.timestamp} Params: ${e.params}`);
+  console.log(`${e.timestamp} Duration: ${e.duration}ms`);
+});
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const { name, email, password } = req.body;
+    try {
+      // Execute a raw SQL query
+      const result =
+        await prisma.$queryRaw`INSERT INTO user (id, name, email, password) VALUES (NULL, ${name}, ${email}, ${password});`;
+
+      console.log(result); // Output the result of the query
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).json({ error });
+    } finally {
+      await prisma.$disconnect(); // Don't forget to disconnect from the database
+    }
+  }
+}
